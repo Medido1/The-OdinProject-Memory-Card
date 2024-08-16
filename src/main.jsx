1b82bb74624a63a8cc8b9daa3d73726f138f9dac
Medido1 <medi1222@gmail.com>
2024-08-16 14:41:03 +0100

    Set up the main structure of the document

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Header from './components/Header'
+import Main from './components/Main'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    <Main />
   </StrictMode>,
 )
