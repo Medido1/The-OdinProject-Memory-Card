3b853c13e785904f34844e6daacf9364c6f1ccb1
Medido1 <medi1222@gmail.com>
2024-08-16 14:44:28 +0100

    Set up score board

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,10 @@ import '../styles/Main.css';
 export default function Main() {
   return (
     <main className="main">
-
+      <div className="score_board">
+        <p className="score">Score: 0</p>
+        <p className="high_score">High Score: 0</p>
+      </div>
     </main>
   )
 }
